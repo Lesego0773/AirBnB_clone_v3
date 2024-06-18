f3283f716a50494fcad3048ed083ac93f5b9d23e
Lesego0773 <kabelomakokga@gmail.com>
2024-06-18 02:17:24 -0700

    init

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,14 +1,25 @@
 #!/usr/bin/python3
-"""Initialize Blueprint views"""
+"""Initialize Flask Blueprint object for API v1."""
 from flask import Blueprint
+from os import getenv
 
-app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
-from api.v1.views.index import *
-from api.v1.views.amenities import *
-from api.v1.views.cities import *
-from api.v1.views.places import *
-from api.v1.views.places_amenities import *
-from api.v1.views.places_reviews import *
-from api.v1.views.states import *
-from api.v1.views.users import *
+# Import all views from the directory
+if getenv('HBNB_TYPE_STORAGE') == 'db':
+    from api.v1.views.index import *
+    from api.v1.views.users import *
+    from api.v1.views.places import *
+    from api.v1.views.amenities import *
+    from api.v1.views.states import *
+    from api.v1.views.cities import *
+    from api.v1.views.places_amenities import *  # Import places_amenities module
+else:
+    from api.v1.views.index import *
+    from api.v1.views.users import *
+    from api.v1.views.places import *
+    from api.v1.views.amenities import *
+    from api.v1.views.states import *
+    from api.v1.views.cities import *
+    from api.v1.views.places_amenities import *  # Import places_amenities module
+
